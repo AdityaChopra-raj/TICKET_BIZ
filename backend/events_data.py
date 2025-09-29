b498beb4b601152c91213fee8412a046717c88d6
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-29 19:47:33 +0530

    Update events_data.py

--- backend/events_data.py
+++ backend/events_data.py
@@ -1,8 +1,30 @@
 import os
-BASE_IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'assets')
-events = {
-    'Navratri Pooja': {'total_tickets':100,'scanned_tickets':0,'image':os.path.join(BASE_IMAGE_PATH,'navratri.jpeg')},
-    'Diwali Dance': {'total_tickets':150,'scanned_tickets':0,'image':os.path.join(BASE_IMAGE_PATH,'diwali.jpeg')},
-    'Freshers': {'total_tickets':200,'scanned_tickets':0,'image':os.path.join(BASE_IMAGE_PATH,'freshers.jpeg')},
-    'Ravan Dehan': {'total_tickets':120,'scanned_tickets':0,'image':os.path.join(BASE_IMAGE_PATH,'ravan_dehan.jpeg')}
-}
+
+BASE_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "assets")
+
+events = [
+    {
+        "name": "Navratri Pooja",
+        "total_tickets": 100,
+        "tickets_scanned": 0,
+        "image": os.path.join(BASE_IMAGE_PATH, "navratri.jpeg")
+    },
+    {
+        "name": "Diwali Dance",
+        "total_tickets": 150,
+        "tickets_scanned": 0,
+        "image": os.path.join(BASE_IMAGE_PATH, "diwali.jpeg")
+    },
+    {
+        "name": "Freshers",
+        "total_tickets": 200,
+        "tickets_scanned": 0,
+        "image": os.path.join(BASE_IMAGE_PATH, "freshers.jpeg")
+    },
+    {
+        "name": "Ravan Dehan",
+        "total_tickets": 80,
+        "tickets_scanned": 0,
+        "image": os.path.join(BASE_IMAGE_PATH, "ravan_dehan.jpeg")
+    }
+]
