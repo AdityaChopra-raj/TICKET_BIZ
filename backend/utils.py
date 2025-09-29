ac3378fb6e94dfd6eb80f88a1d5b39809ad86548
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-29 22:55:55 +0530

    Delete backend/utils.py

--- backend/utils.py
+++
@@ -1,5 +0,0 @@
-def get_event_stats(event_name, events):
-    total = events[event_name]['total_tickets']
-    scanned = events[event_name]['scanned_tickets']
-    remaining = total - scanned
-    return {'total': total, 'scanned': scanned, 'remaining': remaining}
